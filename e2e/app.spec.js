3a6ccb2b161bfdbccdc36eea40e39a1af8bed63b
barkinatc <108333148+barkinatc@users.noreply.github.com>
2024-10-02 11:19:58 +0000

    asf

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -2,8 +2,9 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000'); // Burada uygulamanın adresini yaz
-  await page.waitForLoadState('load');
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Learn Jenkins/);
 });
 
